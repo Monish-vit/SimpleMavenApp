44ba80c206ffab1b1fd7cc4eaa7b0b5faa9e498e
Monish-vit <monishwar.s2023@vitstudent.ac.in>
2026-03-09 10:56:39 +0530

    Add MultiplicationTable class to generate table

---
+++ MultiplicationTable/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Default to 5 if no argument is provided
+        int number = (args.length > 0) ? Integer.parseInt(args[0]) : 5;
+        
+        System.out.println("Multiplication Table for: " + number);
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", number, i, (number * i));
+        }
+    }
+}
